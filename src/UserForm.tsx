62b4d7cc5b79f0dabae05aac5ff212f2b755f7df
fanta-git <fantaji4957t@gmail.com>
2023-04-23 20:11:16 +0900

    react-hookk-formの導入

--- src/UserForm.tsx
+++ src/UserForm.tsx
@@ -1,11 +1,22 @@
+import { useForm } from "react-hook-form";
+
+type Form = {
+  userName: string
+};
+
 function UserForm() {
+  const { register, handleSubmit } = useForm<Form>();
+  const onSubmit = (data: Form) => {
+    console.log(data);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label>ユーザー名</label>
       <br/>
-      <input type="text" />
+      @ <input type="text" {...register('userName')} />
       <br/>
-      <button>選曲</button>
+      <input type="submit" value="選曲" />
     </form>
   );
 }
